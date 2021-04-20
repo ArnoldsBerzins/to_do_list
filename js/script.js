25528a446d705e4cc92019636768384af77cc0cb
ArnoldsBerzins <arnoldsb3@gmail.com>
2021-04-20 15:20:57 +0300

    Lielas izmainas JS, nedaudz HTML u.t.t

--- js/script.js
+++ js/script.js
@@ -4,13 +4,24 @@ let darbi = [];
 window.addEventListener('load', function(e) {
     darbi = JSON.parse(localStorage.getItem("darbi") || "[]");
     console.log(darbi)
-    render();
 });
 
-document.getElementById("pievienotDarbu").addEventListener("click", function(e) {
+document.getElementById("jaunsDarbs").addEventListener("click", function(paradit) {
+    document.getElementById("darbaLapa").style.display = "block";
+});
 
-})
+document.getElementById("pievienosanasPoga").addEventListener("click", function(paslept){
+    document.getElementById("pievienosanasPoga").style.display = "none"
+});
 
-document.getElementById("dzestDarbu").addEventListener("click", function(e) {
+document.getElementById("dzestDarbu").addEventListener("click", function(dzest) {
     div.parentNode.removeChild(div)
-})
+});
+
+document.getElementById("pievienotDarbu").addEventListener("click", function(pievienot) {
+    document.getElementById("pievienosanasPoga").style.display = "flex"
+    document.getElementById("darbaLapa").style.display = "none"
+});
+document.getElementById("pievienotDarbu").addEventListener("click", function(e) {
+    
+});
